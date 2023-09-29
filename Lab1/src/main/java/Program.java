b1e0cc597f8217e0faef53abdee97e588703484a
Danil Demin <prodbykngns@gmail.com>
2023-09-30 00:02:04 +0300

    Added scanner

--- Lab1/src/main/java/Program.java
+++ Lab1/src/main/java/Program.java
@@ -1,5 +1,14 @@
+import java.lang.Math;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Please, enter X:");
+
+        int x = (int) scanner.nextLine();
+
+        scanner.close();
     }
 }
