91b66a06c05a826ddee8d9dafe2cd2b95040f9e8
Danil Demin <prodbykngns@gmail.com>
2023-12-21 07:22:44 +0300

    Fully Completed course work

--- Lab4.5/src/main/java/com/kingnes/servlets/OrderNowServlet.java
+++ Lab4.5/src/main/java/com/kingnes/servlets/OrderNowServlet.java
@@ -76,7 +76,6 @@ public class OrderNowServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
